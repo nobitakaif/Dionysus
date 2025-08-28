611bb2405a86b909f1c2a3cdfef337e7345e4fbc
mohd kaif <mk2818356@gmail.com>
2025-08-28 13:27:55 +0530

    fixed sidebar

--- src/app/(protected)/App-Sidbar.tsx
+++ src/app/(protected)/App-Sidbar.tsx
@@ -1,10 +1,12 @@
 "use client"
 
-import { Bot, CreditCard, LayoutDashboard, Presentation } from "lucide-react"
+import { Bot, CreditCard, LayoutDashboard, Plus, Presentation } from "lucide-react"
+import Image from "next/image"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { it } from "node:test"
-import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "~/components/ui/sidebar"
+import { Button } from "~/components/ui/button"
+import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, useSidebar } from "~/components/ui/sidebar"
 import { cn } from "~/lib/utils"
 
 
@@ -32,13 +34,35 @@ const items = [
 
 ]
 
+const project =[
+    {
+        name : "Project 1"
+    },
+    {
+        name : "Project 1"
+    },
+    {
+        name : "Project 1"
+    },
+    {
+        name : "Project 1"
+    },
+]
 export default function AppSidebar(){
      
     const pathname = usePathname()
+    const { open } = useSidebar();
     
     return <Sidebar collapsible="icon" variant="floating" >
         <SidebarHeader>
-            Logo😭
+            <div className="flex items-center gap-2">
+                    {/* <Image src={} /> */}
+                    { open && <div className="text-xl font-bold">
+                        Dionysus
+                    </div>
+                        
+                    }
+            </div>
         </SidebarHeader>
         <SidebarContent>
             <SidebarGroup>
@@ -59,6 +83,41 @@ export default function AppSidebar(){
                                 </SidebarMenuItem>
                             )
                         })}
+                    </SidebarMenu>
+                </SidebarGroupContent>
+            </SidebarGroup>
+
+            <SidebarGroup>
+                <SidebarGroupLabel>
+                    Your Project
+                </SidebarGroupLabel>
+                <SidebarGroupContent>
+                    <SidebarMenu>
+                        {project.map(pro =>{
+                            return (
+                                <SidebarMenuItem key={pro.name}>
+                                    <SidebarMenuButton asChild>
+                                        <div>
+                                            <div className={cn('rounded-sm border size-6 flex items-center justify-center text-sm text-primary',{
+                                                'bg-primary text-white' : true
+                                            })}>
+                                                {pro.name[0]}
+                                            </div>
+                                            <span>{pro.name}</span>
+                                        </div>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            )
+                        })}
+                        <div className="h-2"></div>
+                        <SidebarMenuItem>
+                            <Link href={"/create"}>
+                                <Button variant={"outline"}  className="w-fit">
+                                    <Plus/>
+                                    Create Project
+                                </Button>
+                            </Link>
+                        </SidebarMenuItem>
                     </SidebarMenu>
                 </SidebarGroupContent>
             </SidebarGroup>
---
+++ src/app/(protected)/qa/page.tsx
@@ -0,0 +1,7 @@
+
+
+export default function QAPage(){
+    return <div>
+        QA page
+    </div>
+}
